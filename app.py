952aac6e52fed550933ec5271fb32ab24f07026d
Benjamin Nour <ben.nour@live.com.au>
2023-04-16 18:07:31 +1000

    added redoc documentation

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ import config
 from models import Character
 
 app = config.connex_app
-app.add_api(config.basedir / "swagger.yml")
+app.add_api(config.basedir / "swagger.yml", options={
+        'swagger_ui': False
+    })
 
 @app.route("/")
 def home():
-    characters = Character.query.all()
-    return render_template("home.html", characters=characters)
+    return render_template("home.html")
+
+
+@app.route("/documentation")
+def documentation():
+    return render_template("documentation.html")
+
+@app.route("/swagger.yml")
+def swagger():
+    return render_template("swagger.yml")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
