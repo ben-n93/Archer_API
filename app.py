7285000bfa8f6d813cc48a487ed1111db91ceb99
Benjamin Nour <ben.nour@live.com.au>
2023-04-20 22:37:24 +1000

    updated html

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def serve_image():
     return send_file(filename, mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, ssl_context='adhoc')
