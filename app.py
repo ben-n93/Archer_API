908623eeebbb9307acc61db0af81c2ef8f48d340
Benjamin Nour <ben.nour@live.com.au>
2024-04-07 23:38:55 +1000

    push

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import render_template, send_file, jsonify
+from flask import render_template, send_file, jsonify, Flask
 import config
 
 app = config.connex_app
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import pathlib
+
 import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -7,7 +8,7 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
-#app.config.update(JSON_SORT_KEYS=False)
+# app.config.update(JSON_SORT_KEYS=False)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'archer_data.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
